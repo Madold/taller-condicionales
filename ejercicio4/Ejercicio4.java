442e56d7ab9b0f2a51a068669e2a2905d750cf80
AndresSuarezz <49999988+AndresSuarezz@users.noreply.github.com>
2022-01-20 19:30:14 -0500

    Add files via upload

---
+++ ejercicio4/Ejercicio4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio4;
+import java.util.Scanner;
+/**
+ *
+ * @author mi pc
+ */
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+       char dato;
+       Scanner Entrada = new Scanner(System.in);
+       vocal Obj = new vocal();
+       
+       System.out.println("Digite una letra:");
+       dato = Entrada.nextLine().charAt(0);
+        
+       Obj.setLetra(dato);
+       Obj.EvaluarLetra();
+   
+    }
+    
+}
